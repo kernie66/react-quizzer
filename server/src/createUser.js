81a7fc41dd98c40fb82cc7d73fb5364b7e69f56b
Kenneth Jagenheim <kenneth.jagenheim@bredband.net>
2023-04-06 09:47:06 +0200

    Got the hang of Sequelize

--- server/src/createUser.js
+++ server/src/createUser.js
@@ -1,24 +1,45 @@
-import { User } from '../models/users.model.js';
+import { User } from '../models/user.model.js';
 import { logger } from './logger/logger.js';
 
-export default async function createUser() {
-  const existingUser = await User.findAll({
-    where: {
-      name: 'Kenneth',
-    },
-  });
+export default async function createUser(userData) {
+  const existingUser = JSON.parse(
+    JSON.stringify(
+      await User.findAll({
+        where: {
+          email: userData.email,
+        },
+      })
+    )
+  );
 
-  if (!existingUser) {
+  logger.info('Logger:', existingUser ? existingUser : 'No data');
+
+  if (existingUser.length === 0) {
+    const lastSeen = new Date();
     try {
-      await User.create({
-        name: 'Kenneth',
-        email: 'kenta@kernie.net',
+      const newUser = await User.create({
+        name: userData.name,
+        email: userData.email,
+        // nicknames: userData.nickname,
+        lastSeen: lastSeen,
       });
-      logger.info('Successfully created user');
+      if (userData.nicknames) {
+        let nicknames = [];
+        nicknames.push(userData.nicknames);
+        newUser.update({
+          nicknames: nicknames,
+        });
+      }
+      logger.info('Successfully created user', userData.name);
+      return true;
     } catch (error) {
       logger.error('Failed to create user:', error);
     }
   } else {
-    logger.info('User already exist');
+    logger.info(
+      'User with same email already exist:',
+      existingUser[0].name
+    );
   }
+  return false;
 }
