19338ec965eeea92fa92d8c1bc7af738e710a2bb
Kenneth Jagenheim <kenneth.jagenheim@bredband.net>
2023-09-02 22:43:50 +0200

    Corrected API error handling for logout

--- server/src/auth/logout.js
+++ server/src/auth/logout.js
@@ -1,12 +1,13 @@
 import { isEmpty } from "radash";
 import { Token } from "../../models/token.model.js";
 import { logger } from "../logger/logger.js";
-import { GeneralError, NotFound } from "../utils/errorHandler.js";
+import { NotFound } from "../utils/errorHandler.js";
 
 export const logout = async (req, res, next) => {
-  if (req.user) {
-    const user = req.user.username;
-    try {
+  try {
+    if (req.user) {
+      const user = req.user.username;
+
       const tokens = await Token.findAll({ where: { userId: req.user.id } });
       if (!isEmpty(tokens)) {
         tokens.map((token) => token.destroy());
@@ -14,13 +15,12 @@ export const logout = async (req, res, next) => {
         res.status(200).json({ success: `User ${user} logged out` });
       } else {
         logger.warn("No token found to delete");
+        throw new NotFound("No token found to destroy...");
       }
-    } catch (error) {
-      logger.error("Error when destroying token:", error);
-      throw new GeneralError("Cannot log out, error deleting user token...");
+    } else {
+      throw new NotFound("User not logged in...");
     }
-  } else {
-    throw new NotFound("User not logged in...");
+  } catch (error) {
+    next(error);
   }
-  next();
 };
