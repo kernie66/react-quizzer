0662378e5b6f7d5fc06745d49fd40e46cf1edd6a
Kenneth Jagenheim <kenneth.jagenheim@bredband.net>
2023-10-24 23:33:54 +0200

    Migrated ChangePassword to Mantine

--- src/pages/ChangePasswordPage.js
+++ src/pages/ChangePasswordPage.js
@@ -1,86 +1,84 @@
-import { useRef, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { Button, Form, FormGroup, Modal, ModalBody, ModalHeader } from "reactstrap";
 import Body from "../components/Body";
-import InputField from "../components/InputField";
 import { useApi } from "../contexts/ApiProvider";
 import { useFlash } from "../contexts/FlashProvider";
 import SetPassword from "../components/SetPassword.js";
 import { useTranslation } from "react-i18next";
+import { useDisclosure, useMediaQuery } from "@mantine/hooks";
+import { useForm } from "@mantine/form";
+import { Button, Divider, Group, Modal, Text, TextInput } from "@mantine/core";
 
 export default function ChangePasswordPage() {
-  const [modal, setModal] = useState(true);
-  const [passwordValue, setPasswordValue] = useState("");
-  const [passwordError, setPasswordError] = useState();
-  const [password2Value, setPassword2Value] = useState("");
-  const [password2Error, setPassword2Error] = useState();
-  const [formErrors, setFormErrors] = useState({});
-  const oldPasswordField = useRef();
-  const newPasswordField = useRef();
-  const newPassword2Field = useRef();
+  const [opened, { close }] = useDisclosure(true);
   const api = useApi();
   const flash = useFlash();
-  const navigate = useNavigate();
   const { t } = useTranslation();
+  const isMobile = useMediaQuery("(max-width: 50em)");
 
-  const onOpened = () => {
-    oldPasswordField.current.focus();
-  };
+  const form = useForm({
+    initialValues: {
+      oldPassword: "",
+      password: "",
+      password2: "",
+    },
+    validate: {
+      oldPassword: (value) => (value.length === 0 ? t("please-enter-your-current-password") : null),
+      password: (value) => (value.length === 0 ? t("please-select-a-password") : null),
+      password2: (value) => (value.length === 0 ? t("please-repeat-the-password") : null),
+    },
+  });
 
-  const onSubmit = async (event) => {
-    event.preventDefault();
-    if (newPasswordField.current.value !== newPassword2Field.current.value) {
-      setFormErrors({ newPassword2: "New passwords don't match" });
+  const onSubmit = async () => {
+    const response = await api.put("/me", {
+      oldPassword: form.values.oldPassword,
+      password: form.values.password,
+    });
+    if (response.ok) {
+      flash(t("your-password-has-been-updated"), "success");
+      close();
+      //        navigate("/me");
     } else {
-      const response = await api.put("/me", {
-        old_password: oldPasswordField.current.value,
-        password: newPasswordField.current.value,
-      });
-      if (response.ok) {
-        setFormErrors({});
-        setModal(false);
-        flash("Your password has been updated", "success");
-        navigate("/me");
-      } else {
-        setFormErrors(response.body.errors.json);
-      }
+      console.log("Error");
     }
   };
 
+  const cancel = () => {
+    close();
+  };
+
   return (
-    <Body sidebar>
-      <Modal isOpen={modal} onOpened={onOpened} fullscreen="sm" className="mt-0">
-        <Form onSubmit={onSubmit}>
-          <ModalHeader className="py-2">{t("change-password")}</ModalHeader>
-          <ModalBody className="pt-0">
-            <FormGroup>
-              <InputField
-                label={t("old-password")}
-                name="oldPassword"
-                type="password"
-                fieldRef={oldPasswordField}
-                error={formErrors.old_password}
-              />
-              <hr />
-              <SetPassword
-                passwordValue={passwordValue}
-                setPasswordValue={setPasswordValue}
-                passwordError={passwordError}
-                setPasswordError={setPasswordError}
-                password2Value={password2Value}
-                setPassword2Value={setPassword2Value}
-                password2Error={password2Error}
-                setPassword2Error={setPassword2Error}
-                passwordField={newPasswordField}
-                password2Field={newPassword2Field}
-                passwordUserInputs={["Saab"]}
-              />
-              <Button color="primary" type="submit">
-                Update
-              </Button>
-            </FormGroup>
-          </ModalBody>
-        </Form>
+    <Body>
+      <Modal
+        opened={opened}
+        onClose={cancel}
+        closeOnEscape={false}
+        centered
+        fullScreen={isMobile}
+        size="lg"
+        title={t("change-password")}
+        mb="xs"
+      >
+        <Divider mb={8} />
+        <Text mb={16} fw={500}>
+          {t("enter-a-new-password-for-your-user")}
+        </Text>
+        <form onSubmit={form.onSubmit(onSubmit)}>
+          <TextInput
+            label={t("old-password")}
+            {...form.getInputProps("oldPassword")}
+            withAsterisk
+            mb="md"
+            data-autofocus={focus}
+            className="CurrentPassword"
+          />
+          <SetPassword form={form} focus={false} />
+          <Divider mb={8} />
+          <Group justify="space-between" my={8} pt={16}>
+            <Button type="submit">{t("update")}</Button>
+            <Button variant="outline" onClick={cancel}>
+              {t("cancel")}
+            </Button>
+          </Group>
+        </form>
       </Modal>
     </Body>
   );
