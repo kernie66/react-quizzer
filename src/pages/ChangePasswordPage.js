bf057a54b8bf372d8cf99e592fcc71691b7781ab
Kenneth Jagenheim <kenneth.jagenheim@bredband.net>
2023-11-03 19:22:57 +0100

    Cleaned up and navigates back

--- src/pages/ChangePasswordPage.js
+++ src/pages/ChangePasswordPage.js
@@ -1,4 +1,3 @@
-import Body from "../components/Body";
 import { useApi } from "../contexts/ApiProvider";
 import SetPassword from "../components/SetPassword.js";
 import { useTranslation } from "react-i18next";
@@ -8,6 +7,7 @@ import { Button, Divider, Group, Modal, PasswordInput, Text, rem } from "@mantin
 import { useUser } from "../contexts/UserProvider.js";
 import { showNotification } from "@mantine/notifications";
 import { IconCheck, IconX } from "@tabler/icons-react";
+import { useNavigate } from "react-router-dom";
 
 export default function ChangePasswordPage() {
   const [opened, { close }] = useDisclosure(true);
@@ -15,6 +15,7 @@ export default function ChangePasswordPage() {
   const { t } = useTranslation();
   const isMobile = useMediaQuery("(max-width: 50em)");
   const { user } = useUser();
+  const navigate = useNavigate();
 
   const form = useForm({
     initialValues: {
@@ -22,16 +23,6 @@ export default function ChangePasswordPage() {
       password: "",
       password2: "",
     },
-    /*    validate: {
-      oldPassword: (value) => (value.length === 0 ? t("please-enter-your-current-password") : null),
-      password: (value, values) =>
-        value.length === 0
-          ? t("please-select-a-password")
-          : value === values.oldPassword
-          ? t("please-enter-a-different-password-than-the-current")
-          : null,
-      password2: (value) => (value.length === 0 ? t("please-repeat-the-password") : null),
-    }, */
     validate: (values) => ({
       oldPassword: values.oldPassword.length === 0 ? t("please-enter-your-current-password") : null,
       password:
@@ -59,7 +50,7 @@ export default function ChangePasswordPage() {
         autoClose: 5000,
       });
       close();
-      //        navigate("/me");
+      navigate(-1);
     } else if (response.status === 200) {
       console.log("Response:", response);
       switch (response.data.error) {
@@ -88,46 +79,45 @@ export default function ChangePasswordPage() {
   };
 
   const cancel = () => {
-    console.log(form.values);
-    // close();
+    close();
+    navigate(-1);
   };
 
   return (
-    <Body>
-      <Modal
-        opened={opened}
-        onClose={cancel}
-        closeOnEscape={false}
-        centered
-        fullScreen={isMobile}
-        size="lg"
-        title={t("change-password")}
-        mb="xs"
-      >
+    <Modal
+      opened={opened}
+      onClose={cancel}
+      closeOnEscape={false}
+      centered
+      fullScreen={isMobile}
+      size="lg"
+      title={t("change-password")}
+      mb="xs"
+    >
+      <Divider mb={8} />
+      <Text mb={16} fw={500}>
+        {t("enter-a-new-password-for-your-user")}
+      </Text>
+      <form onSubmit={form.onSubmit(onSubmit)}>
+        <PasswordInput
+          label={t("old-password")}
+          {...form.getInputProps("oldPassword")}
+          withAsterisk
+          mb="md"
+          data-autofocus
+          autoComplete="current-password"
+          className="CurrentPassword"
+          w="84%"
+        />
+        <SetPassword form={form} focus={false} />
         <Divider mb={8} />
-        <Text mb={16} fw={500}>
-          {t("enter-a-new-password-for-your-user")}
-        </Text>
-        <form onSubmit={form.onSubmit(onSubmit)}>
-          <PasswordInput
-            label={t("old-password")}
-            {...form.getInputProps("oldPassword")}
-            withAsterisk
-            mb="md"
-            data-autofocus
-            className="CurrentPassword"
-            w="84%"
-          />
-          <SetPassword form={form} focus={false} />
-          <Divider mb={8} />
-          <Group justify="space-between" my={8} pt={16}>
-            <Button type="submit">{t("update")}</Button>
-            <Button variant="outline" onClick={cancel}>
-              {t("cancel")}
-            </Button>
-          </Group>
-        </form>
-      </Modal>
-    </Body>
+        <Group justify="space-between" my={8} pt={16}>
+          <Button type="submit">{t("update")}</Button>
+          <Button variant="outline" onClick={cancel}>
+            {t("cancel")}
+          </Button>
+        </Group>
+      </form>
+    </Modal>
   );
 }
