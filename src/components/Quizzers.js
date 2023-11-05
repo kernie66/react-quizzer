d8e13e6b7fb5be08f2719a4a4399f76c6694d72c
Kenneth Jagenheim <kenneth.jagenheim@bredband.net>
2023-11-05 23:01:55 +0100

    Converted Quizzers to Mantine

--- src/components/Quizzers.js
+++ src/components/Quizzers.js
@@ -1,16 +1,21 @@
-import { Alert, Col, Container, Row, Spinner } from "reactstrap";
+// import { Alert, Col, Container, Row, Spinner } from "reactstrap";
 import { useApi } from "../contexts/ApiProvider";
-import Top from "./Top";
 import { useQuery } from "@tanstack/react-query";
 import Quizzer from "./Quizzer.js";
 import { useTranslation } from "react-i18next";
 import getQuizzers from "../helpers/getQuizzers.js";
+import { Affix, Alert, Button, Divider, Loader, Text, Transition, rem } from "@mantine/core";
+import { IconArrowUp, IconInfoCircle } from "@tabler/icons-react";
+import { useWindowScroll } from "@mantine/hooks";
 
 const BASE_API_URL = process.env.REACT_APP_BASE_API_URL;
 
 export default function Quizzers({ currentId }) {
   const api = useApi();
   const { t } = useTranslation();
+  const [scroll, scrollTo] = useWindowScroll();
+
+  const icon = <IconInfoCircle />;
 
   const fetchQuizzers = (id) => {
     return getQuizzers(api, id);
@@ -31,32 +36,44 @@ export default function Quizzers({ currentId }) {
   return (
     <>
       {quizzerError ? (
-        <Alert color="warning">{t("could-not-retrieve-quizzers")}</Alert>
+        <Alert variant="light" color="red" title="Quizzers" icon={icon}>
+          {t("could-not-retrieve-quizzers")}
+        </Alert>
       ) : (
         <>
           {isLoadingQuizzers ? (
             <>
-              <Spinner animation="border" />
-              <p>
+              <Loader color="blue" />
+              <Text span>
                 {t("getting-data-from")} {BASE_API_URL}
-              </p>
+              </Text>
             </>
           ) : (
             <>
               {quizzers.length === 0 ? (
-                <p>{t("there-are-no-quizzers-registered-yet")}</p>
+                <Text>{t("there-are-no-quizzers-registered-yet")}</Text>
               ) : (
-                quizzers.map((quizzer) => <Quizzer key={quizzer.id} quizzer={quizzer} />)
+                quizzers.map((quizzer) => (
+                  <>
+                    <Quizzer key={quizzer.id} quizzer={quizzer} />
+                    <Divider key={quizzer.id} mb={4} />
+                  </>
+                ))
               )}
-              {quizzers.length > 5 ? (
-                <Container fluid>
-                  <Row>
-                    <Col>
-                      <Top />
-                    </Col>
-                    <Col></Col>
-                  </Row>
-                </Container>
+              {quizzers.length > 2 ? (
+                <Affix position={{ bottom: 20, right: 20 }}>
+                  <Transition transition="slide-up" mounted={scroll.y > 0}>
+                    {(transitionStyles) => (
+                      <Button
+                        leftSection={<IconArrowUp style={{ width: rem(16), height: rem(16) }} />}
+                        style={transitionStyles}
+                        onClick={() => scrollTo({ y: 0 })}
+                      >
+                        Scroll to top
+                      </Button>
+                    )}
+                  </Transition>
+                </Affix>
               ) : null}
             </>
           )}
