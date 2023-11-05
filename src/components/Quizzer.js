d9f1a589abeb80aa0ffdf4b152e1e6b921a0d518
Kenneth Jagenheim <kenneth.jagenheim@bredband.net>
2023-11-05 22:35:58 +0100

    Changed Quizzer to Mantine

--- src/components/Quizzer.js
+++ src/components/Quizzer.js
@@ -1,51 +1,42 @@
 import { memo } from "react";
 import { Link } from "react-router-dom";
-import { Container, Row } from "reactstrap";
 import TimeAgo from "./TimeAgo";
 import { useTranslation } from "react-i18next";
 import QuizzerAvatar from "./QuizzerAvatar.js";
+import { Flex, Stack, Text } from "@mantine/core";
 
 export default memo(function Quizzer({ quizzer }) {
   const { t } = useTranslation();
 
   return (
-    <Container fluid className="Quizzer px-0">
-      <div className="d-flex flex-row border-bottom justify-content-start">
-        <div className="Avatar48 ps-0 pe-3 mx-0">
-          <QuizzerAvatar user={quizzer} size={48} />
-        </div>
-        <div className="col">
-          <div className="flex-row">
-            <div className="col">
-              <span>
-                <Link
-                  to={"/user/" + quizzer.id}
-                  className="text-info-emphasis text-decoration-none"
-                >
-                  {quizzer.name}
-                </Link>
-                {quizzer.isAdmin && (
-                  <span className="text-danger">&nbsp;({t("administrator")})</span>
-                )}
-                &nbsp;&mdash;&nbsp;
-                {quizzer.lastPlayed ? (
-                  <>
-                    <span>{t("last-played")} </span>
-                    <TimeAgo isoDate={quizzer.lastPlayed} />
-                  </>
-                ) : (
-                  <span>{t("never-played")}</span>
-                )}
-              </span>
-            </div>
-          </div>
-          <Row className="mb-0">
-            <p className="text-info">
-              {t("email")}: {quizzer.email}
-            </p>
-          </Row>
-        </div>
-      </div>
-    </Container>
+    <Flex justify="flex-start" className="Quizzer">
+      <Text pr="1rem">
+        <QuizzerAvatar user={quizzer} size={48} />
+      </Text>
+      <Stack gap={4} mb="sm">
+        <Text>
+          <Link to={"/user/" + quizzer.id} className="text-info-emphasis text-decoration-none">
+            {quizzer.name}
+          </Link>
+          {quizzer.isAdmin && (
+            <Text span c="red.7">
+              &nbsp;({t("administrator")})
+            </Text>
+          )}
+          &nbsp;&mdash;&nbsp;
+          {quizzer.lastPlayed ? (
+            <>
+              <Text span>{t("last-played")} </Text>
+              <TimeAgo isoDate={quizzer.lastPlayed} />
+            </>
+          ) : (
+            <Text span>{t("never-played")}</Text>
+          )}
+        </Text>
+        <Text c="blue">
+          {t("email")}: {quizzer.email}
+        </Text>
+      </Stack>
+    </Flex>
   );
 });
