72979fc2aa98a46934087eaf286a0de4f894e36d
Kenneth Jagenheim <kenneth.jagenheim@bredband.net>
2023-11-17 22:12:24 +0100

    Made default avatar gradient

--- src/components/UserMenu.js
+++ src/components/UserMenu.js
@@ -2,38 +2,46 @@ import { useUser } from "../contexts/UserProvider";
 import { Avatar, Group, Menu, rem } from "@mantine/core";
 import { IconChevronDown } from "@tabler/icons-react";
 import { quizzerMenuItems } from "../helpers/quizzerMenuItems.js";
+import { useState } from "react";
+import { useShallowEffect } from "@mantine/hooks";
 
 export default function UserMenu() {
   const { user } = useUser();
   const menuItems = quizzerMenuItems(1);
+  const [avatarImage, setAvatarImage] = useState(null);
 
   console.log("User:", user);
 
+  useShallowEffect(() => {
+    if (user && user.avatarUrl) {
+      setAvatarImage(user.avatarUrl + "&s=32");
+    } else {
+      setAvatarImage(null);
+    }
+  }, [user]);
+
   return (
-    <>
-      {!user ? (
-        <Menu trigger="hover" position="bottom-end">
-          <Menu.Target>
-            <Group gap={0}>
-              <Avatar size={32} />
-              <IconChevronDown style={{ width: rem(14), height: rem(14) }} />
-            </Group>
-          </Menu.Target>
-          <Menu.Dropdown>
+    <Menu trigger="hover" position="bottom-end">
+      <Menu.Target>
+        <Group gap={0}>
+          <Avatar
+            src={avatarImage}
+            size={32}
+            variant="gradient"
+            gradient={{ from: "blue", to: "yellow", deg: 320 }}
+          />
+          <IconChevronDown style={{ width: rem(14), height: rem(14) }} />
+        </Group>
+      </Menu.Target>
+      <Menu.Dropdown>
+        {!user ? (
+          <>
             <Menu.Item>{menuItems.loginUser}</Menu.Item>
             <Menu.Divider />
             <Menu.Item>{menuItems.reportIssue}</Menu.Item>
-          </Menu.Dropdown>
-        </Menu>
-      ) : (
-        <Menu trigger="hover" position="bottom-end">
-          <Menu.Target>
-            <Group gap={0}>
-              <Avatar src={user.avatarUrl + "&s=32"} size={32} />
-              <IconChevronDown style={{ width: rem(14), height: rem(14) }} />
-            </Group>
-          </Menu.Target>
-          <Menu.Dropdown>
+          </>
+        ) : (
+          <>
             <Menu.Item>{menuItems.startPage}</Menu.Item>
             <Menu.Item>{menuItems.profile}</Menu.Item>
             {user.isAdmin && <Menu.Item>{menuItems.administer}</Menu.Item>}
@@ -42,9 +50,9 @@ export default function UserMenu() {
             <Menu.Divider />
             <Menu.Item>{menuItems.changePassword}</Menu.Item>
             <Menu.Item>{menuItems.logoutUser}</Menu.Item>
-          </Menu.Dropdown>
-        </Menu>
-      )}
-    </>
+          </>
+        )}
+      </Menu.Dropdown>
+    </Menu>
   );
 }
