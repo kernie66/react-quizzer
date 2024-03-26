052a97596eea042289dd55f39d1bff98c85abf5e
Kenneth Jagenheim <kenneth.jagenheim@bredband.net>
2024-03-26 19:27:21 +0100

    Added error message for SSE

--- src/contexts/SSEProvider.js
+++ src/contexts/SSEProvider.js
@@ -1,13 +1,9 @@
-import { createContext, useContext, useEffect, useState } from "react";
-/*
+import { createContext, useCallback, useContext, useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { showNotification } from "@mantine/notifications";
 import { rem } from "@mantine/core";
 import { TbX } from "react-icons/tb";
-import { useEventSource } from "@react-nano/use-event-source";
-*/
-import { useEventSource } from "react-sse-hooks";
-import { useShallowEffect } from "@mantine/hooks";
+import { useEventSource, useEventSourceListener } from "react-sse-hooks";
 import { useUser } from "./UserProvider.js";
 
 const BASE_API_URL = process.env.REACT_APP_BASE_API_URL;
@@ -16,28 +12,30 @@ const endpoint = BASE_API_URL + "/api/connect";
 const SSEContext = createContext();
 
 export default function SSEProvider({ children }) {
-  // const { t } = useTranslation();
+  const { t } = useTranslation();
   const [url, setUrl] = useState(endpoint);
   const { user } = useUser();
 
-  /*
   const onError = useCallback(() => {
     if (user) {
       showNotification({
         title: t("server-error"),
-        message: "Error connecting to the server sent events",
+        message: t("error-connecting-to-the-server-sent-events"),
         color: "red",
         icon: <TbX style={{ width: rem(18), height: rem(18) }} />,
         autoClose: 8000,
       });
     }
   }, [t]);
-*/
+
   useEffect(() => {
     let newUrl = endpoint;
     if (user) {
       console.log("user.id", user.id);
       newUrl = endpoint + "/" + user.id;
+      startListening();
+    } else {
+      stopListening();
     }
     setUrl(newUrl);
   }, [user]);
@@ -46,13 +44,18 @@ export default function SSEProvider({ children }) {
     source: url,
   });
 
-  useShallowEffect(() => {
-    console.log("Global SSE status changed to:", globalEventSource.readyState);
-    /* if (globalEventSourceStatus === "error") {
-      onError();
-    } */
-  }, [globalEventSource]);
-
+  // eslint-disable-next-line no-unused-vars
+  const { startListening, stopListening } = useEventSourceListener({
+    source: globalEventSource,
+    startOnInit: false,
+    event: {
+      name: "error",
+      listener: (error) => {
+        console.error("Error connecting to global event (SSE):", error);
+        onError();
+      },
+    },
+  });
   return <SSEContext.Provider value={{ globalEventSource }}>{children}</SSEContext.Provider>;
 }
 
