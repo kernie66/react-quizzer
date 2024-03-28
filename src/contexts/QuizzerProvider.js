64b1015457986c881e323d4b2e427d739c2aa14b
Kenneth Jagenheim <kenneth.jagenheim@bredband.net>
2024-03-28 22:50:43 +0100

    Cleanup

--- src/contexts/QuizzerProvider.js
+++ src/contexts/QuizzerProvider.js
@@ -1,16 +1,11 @@
 import { createContext, useContext, useEffect, useState } from "react";
-import { useApi } from "./ApiProvider";
-import { useErrorBoundary } from "react-error-boundary";
-import { useLoggedInQuery } from "../hooks/useLoginQuery.js";
-import { useSetState, useShallowEffect } from "@mantine/hooks";
+import { useSetState } from "@mantine/hooks";
 import { useEventSourceListener } from "react-sse-hooks"; // "@react-nano/use-event-source";
 import { useSSE } from "./SSEProvider.js";
 
 const QuizzerContext = createContext();
 
 export default function QuizzerProvider({ children }) {
-  const api = useApi();
-  const { showBoundary } = useErrorBoundary();
   const { globalEventSource } = useSSE();
   const [quizzers, setQuizzers] = useSetState({ quizMaster: [], quizzers: [] });
   const [clients, setClients] = useState(0);
@@ -40,39 +35,6 @@ export default function QuizzerProvider({ children }) {
   useEffect(() => {
     setClients(quizzers.quizzers.length + quizzers.quizMaster.length);
   }, [quizzers]);
-
-  const {
-    isLoading: isLoadingLoggedIn,
-    isError: isErrorLoggedIn,
-    data: loggedInId,
-  } = useLoggedInQuery();
-
-  useShallowEffect(() => {
-    //let userId = 0;
-    // Check if the user has been logged in
-    if (api.isAuthenticated()) {
-      // Check if the login is still valid
-      try {
-        if (loggedInId) {
-          console.log("Logged in quizzer ID:", loggedInId);
-          console.log("Start listening for quizzers");
-          startListening();
-        } else if (isLoadingLoggedIn) {
-          console.log("Loading login info");
-        } else if (isErrorLoggedIn) {
-          console.error("Error checking logged in user");
-        } else {
-          console.log("Remove login");
-          api.removeLogin();
-        }
-      } catch (error) {
-        console.error("Error checking login:", error);
-        showBoundary(error);
-      }
-    } else {
-      stopListening();
-    }
-  }, [api, loggedInId, isErrorLoggedIn, isLoadingLoggedIn]);
 
   return (
     <QuizzerContext.Provider value={{ quizzers, clients }}>{children}</QuizzerContext.Provider>
